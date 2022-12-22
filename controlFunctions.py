721a29e5990ee7981021376009353e127f1c67c6
archiron <arnaud.chiron@cern.ch>
2022-12-22 10:26:30 +0100

    version 2022.12.22-1
    small correction in change_nbFiles function : format output

--- controlFunctions.py
+++ controlFunctions.py
@@ -148,7 +148,7 @@ def optimizeBranches(tmp_branches):
 def change_nbFiles(nbFiles_computed, nbFiles):
     if (nbFiles_computed != nbFiles):
         print('the number of computed files (' + '{:d}'.format(nbFiles_computed) + ') is different from the pre supposed number (' + '{:d}'.format(nbFiles) + ').')
-        print('switching to the computed number.')
+        print('switching to the computed number ({:d}).'.format(nbFiles_computed))
         return nbFiles_computed
     else:
         return nbFiles
